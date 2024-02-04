3791d83a8d6a413076ccc3bc6905f2b2965d168e
El-swaggerito <95658387+El-swaggerito@users.noreply.github.com>
2024-02-04 15:49:00 +0100

    Completed Project

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-import { Button } from "../../../snapcode/components/ui/button";
 import LogoutButton from "./logout.button";
+import { Button } from "../ui/button";
 
 const Navbar = async () => {
   return (
